83b4aa1fb2aabfbf169ebb102680020ff98fc494
Yuanyi Wang <54620490+wyy-code@users.noreply.github.com>
2024-05-04 12:12:55 +0800

    Update run_PSM.sh

--- script/run_PSM.sh
+++ script/run_PSM.sh
@@ -1,6 +1,7 @@
 CUDA_VISIBLE_DEVICES=0 python main.py\
     --n_blocks=2\
     --batch_size=256\
-    --window_size=6\
+    --window_size=8\
+    --stride_size=8\
     --train_split=0.6\
     --name=PSM
